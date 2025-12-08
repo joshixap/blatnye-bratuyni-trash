1a07d02e053f5776465b55e3cbf021f21bec40e6
joshi <g00gleisdork@gmail.com>
2025-12-08 20:06:39 +0300

    delete all

--- services/notification-service/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-SMTP_SERVER = os.getenv("SMTP_SERVER")
-SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
-EMAIL_USER = os.getenv("EMAIL_USER")
-EMAIL_PASS = os.getenv("EMAIL_PASS")
-# TODO: queue parameters placeholder
--- services/user-service/tests/test_health.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-def test_health_check(test_client):
-    """Test that the service is running"""
-    # User service doesn't have a root endpoint, so we'll just test imports work
-    assert test_client is not None
